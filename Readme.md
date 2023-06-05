3596aec13e52aec302215f7f229ed0466d3ba97b
Jib <jibrilhart3@gmail.com>
2023-06-05 18:16:32 -0400

    Added a cute lil read me

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# What is a Zine?
+zines are cute little comics that you can print out, and fold horizontally to form a physical booklet. Because of this you will see that in a zine the first pdf page is split in two. With the top part being the first page, and the bottom part being the last page. While this can be pretty handy if you want to print it out and have it near you physically for refference, its convinent for reading normally with a pdf viewer. So using this tool you can convert them to a layout thats easier to read.
+
+
+# Installation
+The easiest way to install is to install as a python package, like so:
+``` bash
+pip install unzine
+```
+
+**Maybe add a way to install with git?**
+
+# Usage
+
+```bash
+unzine target.pdf
+```
+
--- zineUnwind.py
+++ zineUnwind.py
@@ -1,13 +1,14 @@
+from copy import deepcopy
+from pathlib import Path
+from sys import argv
+#from optparse import OptionParser
+
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from pdf2image import convert_from_path
 import img2pdf
 import PIL
+# from scipy import misc
 
-from copy import deepcopy
-#from scipy import misc
-
-
-from pathlib import Path
 
 def rotate_pages(pdf_path):
     pdf_writer = PdfFileWriter()
@@ -24,60 +25,78 @@ def rotate_pages(pdf_path):
     with open('rotate_pages.pdf', 'wb') as fh:
         pdf_writer.write(fh)
 
-def test(pdpath:Path):
+
+def test(pdpath: Path):
     images = convert_from_path(pdpath.absolute())
     for i, page in enumerate(images):
         page.save(f"./out/page{str(i)}.jpg")
 
-def readzine(deckpath:Path):
+def splitHorizontal(page: PIL.Image):
+    width, height = page.size
+    width_cutoff = page.width // 2
+
+    first = deepcopy(page.crop((0, 0, width_cutoff, height)))
+    last = deepcopy(page.crop((width_cutoff, 0, width, height)))
+    return(first, last)
+
+def readzine(deckpath: Path):
     deck = convert_from_path(deckpath.absolute())
     name = deckpath.name
     fromStart = []
     fromBack = []
-    for i, page in enumerate(deck):
-        width, height = page.size
-        width_cutoff = page.width // 2
+
+    first, last = splitHorizontal(deck[0])  # first page is folded different
+    fromStart.append(first.rotate(90, PIL.Image.NEAREST, expand=1))
+    fromBack.append(last.rotate(-90, PIL.Image.NEAREST, expand=1))
+    for i, page in enumerate(deck[1:]):
+
 
         if (i % 2):
-            first = deepcopy(page.crop((0, 0, width_cutoff, height)))
-            last = deepcopy(page.crop((width_cutoff, 0, width, height)))
-
-            fromStart.append(first.rotate(-90, PIL.Image.NEAREST, expand = 1))
-            fromBack.append(last.rotate(-90, PIL.Image.NEAREST, expand = 1))
+            first, last = splitHorizontal(page)
+            fromStart.append(first.rotate(90, PIL.Image.NEAREST, expand=1))
+            fromBack.append(last.rotate(90, PIL.Image.NEAREST, expand=1))
         else:
-            first = deepcopy(page.crop((0, 0, width_cutoff, height)))
-            last = deepcopy(page.crop((width_cutoff, 0, width, height)))
-
-            fromStart.append(first.rotate(90, PIL.Image.NEAREST, expand = 1))
-            fromBack.append(last.rotate(90, PIL.Image.NEAREST, expand = 1))
-
-
+            first, last = splitHorizontal(page)
+            fromStart.append(first.rotate(-90, PIL.Image.NEAREST, expand=1))
+            fromBack.append(last.rotate(-90, PIL.Image.NEAREST, expand=1))
 
     fromBack.reverse()
-    #orderdDeck = fromStart.extend(fromBack)  # make sure reverse is right, pages are still rotated
+    # orderdDeck = fromStart.extend(fromBack)  # make sure reverse is right, pages are still rotated
     fromStart.extend(fromBack)
-    #pdf_bytes = img2pdf.convert(orderdDeck)
+    # pdf_bytes = img2pdf.convert(orderdDeck)
 
-    #for i, page in enumerate(fromStart):
+    # for i, page in enumerate(fromStart):
     #    page.save(f"./out/page{str(i)}.jpg")
     fromStart[0].save(f'{name}', save_all=True, append_images=fromStart[1:])
 
+    # file = open(f"{name}.pdf", "wb")
 
-    #file = open(f"{name}.pdf", "wb")
-  
     # writing pdf files with chunks
-    #file.write(pdf_bytes)
-
+    # file.write(pdf_bytes)
 
 
 if __name__ == '__main__':
-    path = Path('./linuxZines')
-    assert path.exists()
-    for pdf in path.iterdir():
-        # spawn a new thread for each thing
-        readzine(pdf)
 
-# last page is busted
+
+    if len(argv) == 2:
+        path = Path(argv[1])
+    else:
+        print("Program can only have one input. \nExample: python3 zineUnwind.py target")
+        exit(1)
+
+    assert path.exists()
+
+    if path.is_dir():
+        for file in path.iterdir():
+            # spawn a new thread for each thing
+            if file.suffix == '.pdf':
+                readzine(file)
+
+    else:
+        if path.suffix == 'pdf':
+            readzine(path)
+
+# triple check zine formatting
 # super slow
 # add tests?
 # add cli
